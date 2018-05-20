c84bfd8732f5442dc310fe732f9ed7c1adc2af80
federicogiovannini <federico.giovannini18@gmail.com>
2018-05-20 18:05:37 +0200

    q

--- source/botDemo.py
+++ source/botDemo.py
@@ -1,4 +1,4 @@
-import telegram 
+import telegram
 
 # Start
 TOKEN = "607283694:AAGk1aiS_11-c-cYHSwGVbfM34bb4TJ-x-4"
@@ -22,4 +22,3 @@ start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
 
 updater.start_polling()
-
