8b13b5b7fb6d39f624b00773553412352b7b054e
federicogiovannini <federico.giovannini18@gmail.com>
2018-05-20 23:49:17 +0200

    qwe

--- source/botDemo.py
+++ source/botDemo.py
@@ -1,11 +1,32 @@
 import telegram
-import telegram.ext 
+
+AUDIO_URL = "http://audio.radio24.ilsole24ore.com/radio24_audio/2018/180518-lazanzara-s.mp3"
 
 # Start
 TOKEN = "607283694:AAGk1aiS_11-c-cYHSwGVbfM34bb4TJ-x-4"
 
+
+def build_menu(buttons,
+               n_cols,
+               header_buttons=None,
+               footer_buttons=None):
+    menu = [buttons[i:i + n_cols] for i in range(0, len(buttons), n_cols)]
+    if header_buttons:
+        menu.insert(0, header_buttons)
+    if footer_buttons:
+        menu.append(footer_buttons)
+    return menu
+
+
 def start(bot, update):
-	bot.send_message(chat_id = update.message.chat_id, text = "Ecco l'ultimo podcast disponibile")
+	AUDIO_URL = "http://audio.radio24.ilsole24ore.com/radio24_audio/2018/180518-lazanzara-s.mp3"
+	episodes_list = [ AUDIO_URL, ]
+	button_list = [ [InlineKeyboardButton( text=(s.split("/")[-1] or '').split("-")[0] or s, url=s )] for s in episodes_list ]
+
+	reply_markup = InlineKeyboardMarkup( util.build_menu( button_list, n_cols=1) )
+
+	bot.send_message( chat_id=update.message.chat_id, text="Ecco gli ultimi podcast disponibili", reply_markup=reply_markup )
+
 
 
 from telegram.ext import Updater
