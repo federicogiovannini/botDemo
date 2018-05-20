d44d3cabbc18086347f9dfab99aae85b720ff45a
federicogiovannini <federico.giovannini18@gmail.com>
2018-05-20 17:30:48 +0200

    a

--- source/botDemo.py
+++ source/botDemo.py
@@ -3,6 +3,9 @@ import telegram
 # Start
 TOKEN = "607283694:AAGk1aiS_11-c-cYHSwGVbfM34bb4TJ-x-4"
 
+bot = telegram.Bot(token = TOKEN)
+
+
 
 def start(bot, update):
 	bot.send_message(chat_id = update.message.chat_id, text = "Ecco l'ultimo podcast disponibile")
