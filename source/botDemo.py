1bdd9392061f003727be47313a4ea47c346074ab
federicogiovannini <federico.giovannini18@gmail.com>
2018-05-20 17:49:23 +0200

    speriamo

--- source/botDemo.py
+++ source/botDemo.py
@@ -1,11 +1,9 @@
-import telegram 
+#import telegram 
 
 # Start
 TOKEN = "607283694:AAGk1aiS_11-c-cYHSwGVbfM34bb4TJ-x-4"
 
 bot = telegram.Bot(token = TOKEN)
-
-
 
 def start(bot, update):
 	bot.send_message(chat_id = update.message.chat_id, text = "Ecco l'ultimo podcast disponibile")
@@ -24,3 +22,4 @@ start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
 
 updater.start_polling()
+
